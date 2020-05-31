ea97357114316b117352d345556ffee4538c3b6c
CrepeGoat <awqatty.b@gmail.com>
2020-05-31 11:13:58 -0400

    tmp: filling in code

--- linter.py
+++ linter.py
@@ -1,10 +1,21 @@
+import re
+
 from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
+OUTPUT_RE = re.compile(
+    r"<stdin>:(?P<line>\d+):((?P<col>\d+):)?\s*"
+    r".*?((?P<error>error)|(?P<warning>warning|note)):\s*"
+    r"(?P<message>.+)",
+    re.MULTILINE,
+)
+
+
+class Rust(Linter):
+    cmd = 'cargo clippy ${args} -'
     regex = r''
     multiline = False
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.rust',
+        'args': [],
     }
